c891dee8e98ad877dd265da113792de67f55e126
srishti <srishtiprasad500@gmail.com>
2024-06-21 14:34:26 +0530

    unique bst and target sum pf type merge interval

--- dynamicProgramming/README.md
+++ dynamicProgramming/README.md
@@ -7,11 +7,28 @@ Patterns of questions in dynamic programming
    Brute force approach : finding min/max path among all possible paths before the current state, then add value for the current state.
 
    we use recursion for this & sometime solution set will be given to us in question itself.
-   For example : minCostClimbStairs , minPathSum
+   For example : minCostClimbStairs , minPathSum,minFallingPathSum,coinChange 
 
    routes[i] = min(routes[i-1], routes[i-2], ... , routes[i-k]) + cost[i]
 2. Distinct Ways
+   Problem List: https://leetcode.com/list/55ajm50i
+
+   **Statement**
+      Given a target find a number of distinct ways to reach the target.
+   
+   For example: uniquePath , target sum,
 3. Merging Intervals
+   Problem List: https://leetcode.com/list/55aj8s16
+
+   **Problem Statement**
+   
+   Given a set of numbers find an optimal solution for a problem considering the current number and the best you can get from the left and right sides.
+
+   **Approach**
+   Find all optimal solutions for every interval and return the best possible answer.
+
+   For example:   Unique binary Search Trees 
+   
 4. DP on Strings
 5. Decision Making
 
---
+++ dynamicProgramming/uniqueBST.cpp
@@ -0,0 +1,56 @@
+//MERGE INTERVALS
+
+//RECURSIVE APPROACH 
+//TC : O(3^N)
+class Solution {
+public:
+    int numTrees(int n) {
+        if(n<=1){
+            return 1;
+        }
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans+= numTrees(i-1)*numTrees(n-i);
+        }
+        return ans;
+    }
+};
+
+//MEMOISED APPROACH 
+//TC : O(N*N)
+class Solution {
+public:
+    int dp[20]={0}; 
+    int numTrees(int n) {
+        if(n<=1){
+            return 1;
+        }
+        if(dp[n]){
+            return dp[n];
+        }
+        for(int i=1;i<=n;i++){
+            dp[n]+= numTrees(i-1)*numTrees(n-i);
+        }
+        return dp[n];
+    }
+};
+/*
+Recursion Tree
+n = 3
+
+root        1           2           3
+        i-1 n-i        1  1         2  0
+        0   2          1  3         
+
+
+root        1           2           3           4           5
+i-1 n-i   0  4         1  3       2   2       3  1         4  0
+            3 1
+*/
+                   
+        1               2            2
+          5            1 3         1    5
+         4                4            3
+        3                 5           4
+       2 
+
