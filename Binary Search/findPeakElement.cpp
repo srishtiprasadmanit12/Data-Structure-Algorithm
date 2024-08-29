e3d3c4caab6205a019750bc96dbbb720a43b9ba3
srishti <srishtiprasad500@gmail.com>
2024-08-29 20:38:14 +0530

    number of enclave

--- Binary Search/findPeakElement.cpp
+++ Binary Search/findPeakElement.cpp
@@ -1,3 +1,29 @@
+/*************************BRUTE FORCE APPROACH************************* */ 
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+    int n = nums.size();
+    if(n==1){
+        return 0;
+    } 
+    if(nums[0]>nums[1]){
+        return 0;
+    }
+    if(nums[n-1]>nums[n-2]){
+        return n-1;
+    }
+    int ans = 0;   
+    //O(N)
+    for(int i=1;i<n-1;i++){
+        if(nums[i-1]<nums[i] && nums[i]>nums[i+1]){
+            ans = i;
+            break;
+        }
+    }
+    return ans;
+    }
+};
+/**************************OPTIMISED O(logn)************************** */
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
--- graphs/dfs/NumberOfEnclaves.cpp
+++ graphs/dfs/NumberOfEnclaves.cpp
@@ -1,41 +1,46 @@
+/*
+Intuition
+So, the intuition behind this problem is that we have to go through all the 1's from the border only that are present in the matrix.
+So, I decided to do a simple dfs from the border of the matrix only
+
+Approach
+In order to approach this problem, I have done a simple dfs from the border of the matrix.
+Firstly, I will make a visited matrix which we normally create while doing a graph problem.
+Then I only traverse on borders of the matrix and see if I'll encounter a '1' which is not visited, then I'll mark it visited and also its neighbouring '1's visited because we can reach them from the border.
+Same I will do in all the Four directions.
+*/
+
 class Solution {
 public:
-    void check(vector<vector<int>>&grid,vector<vector<int>>&visited,int &count,int &flag,int i,int j){
-        // Validity check
-        if(i<0 || j<0 || j>grid[0].size()|| i>grid.size() || grid[i][j] == 0 || visited[i][j]){
+    void dfs(int i,int j,int r,int c,vector<vector<int>>&grid,vector<vector<int>>&vis){
+
+        if(i<0 ||j<0||i==r||j==c || vis[i][j]||!grid[i][j]){
             return;
         }
-        // if the node is boundary node having value=1
-        if(i==0 || j==0 || i == grid.size()-1 || j == grid[0].size()-1){
-            flag = 1;
-        }
-        count+=1;
-        visited[i][j]=1;
+        vis[i][j]=1;
 
-        check(grid,visited,count,flag,i-1,j);
-        check(grid,visited,count,flag,i+1,j);
-        check(grid,visited,count,flag,i,j-1);
-        check(grid,visited,count,flag,i,j+1);
+             dfs(i+1,j,r,c,grid,vis);
+             dfs(i-1,j,r,c,grid,vis);
+             dfs(i,j+1,r,c,grid,vis);
+             dfs(i,j-1,r,c,grid,vis);
     }
     int numEnclaves(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>visited(n,vector<int>(m,0));
-        int count = 0;
-        int ans = 0;
-
-        int flag = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!visited[i][j] && grid[i][j]==1){
-                    int count = 0 , flag = 0;
-                    check(grid,visited,count,flag,i,j);
-                    if(flag==0){
-                        ans+=count;
-                    }
+        int r = grid.size();int c = grid[0].size();
+        vector<vector<int>>vis(r,vector<int>(c,0));
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if((i==0||i==r-1||j==0||j==c-1)&&grid[i][j]==1){
+                    dfs(i,j,r,c,grid,vis);
                 }
             }
         }
-        return ans;
+        int cnt=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j] == 1 && !vis[i][j])
+                    cnt++;
+            }
+        }
+        return cnt;
     }
 };
