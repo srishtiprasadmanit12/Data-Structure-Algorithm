7a1ca5b2551c5f7c7018d48301e4dc27254d3e36
srishti <srishtiprasad500@gmail.com>
2024-09-15 17:01:18 +0530

    generate parenthesis backtracking

---
+++ backtracking/generateParenthesis.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void solve(int n, int open, int close, string output, vector<string>&ans){
+        if(open == n && close == n){
+            ans.push_back(output);
+            return;
+        }
+
+        if(open!=n){
+            solve(n,open+1,close,output+"(",ans);
+        }
+        if(open>close){
+            solve(n,open,close+1,output+")",ans);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        string output;
+        solve(n,0,0,output,ans);
+        return ans;
+    }
+};
