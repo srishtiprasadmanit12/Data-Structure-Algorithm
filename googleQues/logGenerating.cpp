fd03ca7ed9b437657f585a5e10ebd2e881dde759
srishti <srishtiprasad500@gmail.com>
2026-04-19 16:47:01 +0530

    google ques

--- googleQues/logGenerating.cpp
+++ googleQues/logGenerating.cpp
@@ -1,10 +1,46 @@
+
 class log{
     int timestamp;
     string message;
+
+    // constructor
+    Log(int t,string m){
+        timestamp = t;
+        message = m;
+    }
+};
+/*
+bool customComparison(int a, int b)
+{
+    // Custom comparison logic
+    return a < b; // it sorts in ascending order
+}
+*/
+class compare{
+    publc:
+        bool operator()(const Log &a,const Log &b){
+            return a.timestamp > b.timestamp
+        }
 };
 
-log getLog(); // api to generate the log
+class LogStorage{
+    private :
+        priority_queue<Log,vector<Log>,compare>pq;
+    public:
+        void addLog(Log log){
+            pq.push(log);
+        }
+    vector<string>getLastKMessages(int k){
+        vector<string>result;
 
-int main(){
-    log log1 = getLog(); // log1 is a log object
-}
+        //copy heap so original remains intact
+        auto temp = pq;
+        while(k-- && !temp.empty()){
+            result.push_back(temp.top().message);
+            temp.pop();
+        }
+        return result;
+    }
+};
+
+
